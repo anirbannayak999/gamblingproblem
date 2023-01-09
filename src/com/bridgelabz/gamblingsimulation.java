02c9ae6b2c6a89e016133e1acf5e54ecd116ae8e
anirbannayak999 <anirbannayak999@gmail.com>
2023-01-09 15:54:47 +0530

    checking resign

--- src/com/bridgelabz/gamblingsimulation.java
+++ src/com/bridgelabz/gamblingsimulation.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 public class gamblingsimulation {
 
+
     static int stake = 100;
     static final int BET = 1;
 
@@ -21,10 +22,28 @@ public class gamblingsimulation {
         }
     }
 
+    /* (Single day gambling process) Gambler will resign for the day if won or lost 50% of the stake */
+    public static void gambling() {
+        double winningTarget = stake * 1.5;
+        double loosingTarget = stake * 0.5;
+
+        System.out.println("Starting with : " +stake + "    Winning target = " + winningTarget + "    Loosing target = " + loosingTarget);
+
+        while(stake <= winningTarget && stake >= loosingTarget) {
+            checkWinOrLoose();
+            System.out.println("\n");
+
+            if(stake == 150 || stake == 50) {
+                System.out.println("***************************** Target Achieved  *******************************");
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println("***************** WELCOME TO GAMBLING SIMULATOR PROBLEM ****************\n");
-        checkWinOrLoose();
+        gambling();
     }
 
 }
