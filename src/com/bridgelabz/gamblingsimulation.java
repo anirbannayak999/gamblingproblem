fb272ffff7bf69fa3dff8dc4513702a86676d7f5
anirbannayak999 <anirbannayak999@gmail.com>
2023-01-09 15:39:07 +0530

    welcome to gambling problem

---
+++ src/com/bridgelabz/gamblingsimulation.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class gamblingsimulation {	static int stake = 100;
+    static final int BET = 1;
+
+    public static void main(String[] args) {
+
+        System.out.println("***************** WELCOME TO GAMBLING SIMULATOR PROBLEM ****************\n");
+    }
+
+
+
+
+
+
+}
